4ad8fa50492214de19d725ddfea58341fc01aea4
Arlefreak <arlefreak@gmail.com>
2015-12-30 00:20:10 -0600

    Fixed index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-var test = true;
-console.log(test);
+'use strict';
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    port = parseInt(process.env.PORT, 10) || 8000;
+
+app.get("/", function(req, res) {
+    res.redirect("index.html");
+});
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler({
+    dumpExceptions: true,
+    showStack: true
+}));
+
+app.listen(port);
