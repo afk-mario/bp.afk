2dc57b2730d86557b8589495a7e9e25203b9c957
Arlefreak <hi@arlefreak.com>
2016-12-19 11:51:44 -0600

    Changed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 
 const gulp        = require('gulp');
 const gutil       = require('gulp-util');
-const source       = require('vinyl-source-stream');
+const source      = require('vinyl-source-stream');
 const browserify  = require('browserify');
 const babelify    = require('babelify');
 const runSequence = require('run-sequence');
 const bower       = require('main-bower-files');
 const gulpif      = require('gulp-if');
-const jshint      = require('gulp-jshint');
+const lint        = require('gulp-eslint');
 const uglify      = require('gulp-uglify');
 const concat      = require('gulp-concat');
 const flatten     = require('gulp-flatten');
@@ -18,24 +18,30 @@ const nib         = require('nib');
 const stylus      = require('gulp-stylus');
 const connect     = require('gulp-connect');
 const imagemin    = require('gulp-imagemin');
+const cleanCSS    = require('gulp-clean-css');
 const pngquant    = require('imagemin-pngquant');
 const gifsicle    = require('imagemin-gifsicle');
 const jpegtran    = require('imagemin-jpegtran');
 const svgo        = require('imagemin-svgo');
 
 const DEBUG = process.env.NODE_ENV === 'production' ? false : true;
+const DEST_PATH = 'public/';
+const DEST_PATH_LIB = DEST_PATH + 'lib/';
 
 // grab libraries files from bower_components, minify and push in /public
-gulp.task('bower', function() {
-    // var filter = {};
-    var jsFilter = gulpFilter('**/*.js', {
+gulp.task('bower', ()=> {
+    var filterJs = gulpFilter('**/*.js', {
         restore: true
     });
-    var cssFilter = gulpFilter('*.css', {
+    var filterCss = gulpFilter('**/*.css', {
         restore: true
     });
-    var fontFilter = gulpFilter(['*.eot', '*.woff', '*.svg', '*.ttf']);
-    var destPath = 'public/lib';
+    var filterFont = gulpFilter(['**/*.eot', '**/*.woff', '**/*.ttf'],{
+        restore: true
+    });
+    var filterImg = gulpFilter(['**/*.jpg', '**/*.png', '**/*.svg', '**/*.jpeg', '**/*.gif'],{
+        restore: true
+    });
 
     return gulp.src(bower({
         debugging: true,
@@ -43,33 +49,36 @@ gulp.task('bower', function() {
     }))
 
     // grab vendor js files from bower_components, minify and push in /public
-    .pipe(jsFilter)
-    .pipe(gulp.dest(destPath + '/js/'))
+    .pipe(filterJs)
     .pipe(gulpif(!DEBUG, uglify()))
-    .pipe(concat('vendor.js'))
     .pipe(rename({
         suffix: '.min'
     }))
-    .pipe(gulp.dest(destPath + '/js/'))
-    .pipe(jsFilter.restore)
+    .pipe(gulp.dest(DEST_PATH_LIB + 'js/'))
+    .pipe(filterJs.restore)
 
     // grab vendor css files from bower_components, minify and push in /public
-    .pipe(cssFilter)
-    .pipe(gulp.dest(destPath + '/css'))
-    // .pipe(minifycss())
+    .pipe(filterCss)
+    .pipe(gulp.dest(DEST_PATH_LIB + 'css/'))
+    .pipe(gulpif(!DEBUG, cleanCSS({compatibility: 'ie8'})))
     .pipe(rename({
         suffix: '.min'
     }))
-    .pipe(gulp.dest(destPath + '/css'))
-    .pipe(cssFilter.restore)
+    .pipe(gulp.dest(DEST_PATH_LIB + 'css/'))
+    .pipe(filterCss.restore)
 
     // grab vendor font files from bower_components and push in /public
-    .pipe(fontFilter)
+    .pipe(filterFont)
     .pipe(flatten())
-    .pipe(gulp.dest(destPath + '/fonts'));
+    .pipe(gulp.dest(DEST_PATH_LIB + 'fonts/'))
+
+    // grab vendor font files from bower_components and push in /public
+    .pipe(filterImg)
+    .pipe(flatten())
+    .pipe(gulp.dest(DEST_PATH_LIB + 'img/'));
 });
 
-gulp.task('browserify', function() {
+gulp.task('browserify', ()=> {
     return browserify()
     .on('error', gutil.log)
     .require('./src/js/app.js', {
@@ -82,59 +91,58 @@ gulp.task('browserify', function() {
     })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./public/js'));
+    .pipe(gulp.dest(DEST_PATH + 'js/'));
 });
 
-gulp.task('js', function() {
+gulp.task('js', ()=> {
     return gulp.src(['src/js/**/*.js', '!src/js/templates/**/*.js'])
-    .pipe(jshint({
-        devel: DEBUG
-    }))
-    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(lint())
+    .pipe(lint.format())
+    .pipe(lint.failAfterError())
     .pipe(gulpif(!DEBUG,uglify()))
     .pipe(concat('script.js'))
-    .pipe(gulp.dest('./public/js/'))
+    .pipe(gulp.dest(DEST_PATH + 'js/'))
     .pipe(connect.reload());
 });
 
-gulp.task('css', function() {
+gulp.task('css', ()=> {
     gulp.src('src/css/style.styl')
     .pipe(stylus({
         use:nib(),
         compress: !DEBUG,
         import:['nib']
     }))
-    .pipe(gulp.dest('public/css/'))
+    .pipe(gulp.dest(DEST_PATH + 'css/'))
     .pipe(connect.reload());
 });
 
-gulp.task('img', function(){
+gulp.task('img', ()=>{
     gulp.src('src/img/**/*')
     .pipe(imagemin({
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         use: [pngquant(), gifsicle(), jpegtran(), svgo()]
     }))
-    .pipe(gulp.dest('public/img'))
+    .pipe(gulp.dest(DEST_PATH + 'img/'))
     .pipe(connect.reload());
 });
 
-gulp.task('html', function() {
+gulp.task('html', ()=> {
     gulp.src('./src/*.html')
-    .pipe(gulp.dest('./public/'))
+    .pipe(gulp.dest(DEST_PATH))
     .pipe(connect.reload());
 });
 
-gulp.task('files', function() {
+gulp.task('files', ()=> {
     gulp.src(['./src/**.*', '!./src/**.*.html', '!./src/**.*js', '!./src/img/'])
     .pipe(gulp.dest('./public/'));
 });
 
-gulp.task('react', function() {
+gulp.task('react', ()=> {
     runSequence('js', 'browserify');
 });
 
-gulp.task('connect', function() {
+gulp.task('connect', ()=> {
     connect.server({
         root: 'public',
         livereload: true,
@@ -143,7 +151,7 @@ gulp.task('connect', function() {
 
 gulp.task('init', ['css', 'bower', 'react', 'img', 'html', 'files']);
 
-gulp.task('watch', ['css', 'react', 'img', 'html', 'connect'], function() {
+gulp.task('watch', ['css', 'react', 'img', 'html', 'connect'], ()=> {
     gulp.watch('src/css/**/*.styl', ['css']);
     gulp.watch('src/js/**/*.js', ['react']);
     gulp.watch('src/js/**/*.jsx', ['react']);
@@ -151,6 +159,6 @@ gulp.task('watch', ['css', 'react', 'img', 'html', 'connect'], function() {
     gulp.watch('src/*.html', ['html']);
 });
 
-gulp.task('default', function() {
+gulp.task('default', ()=> {
     runSequence('init', 'watch');
 });
